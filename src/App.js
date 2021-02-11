6f67690eb6698ef4f9beed3882a0625eb67c2f7c
Andrei Surzhan <surzhan.a.y@gmail.com>
2021-02-10 21:53:28 -0500

    remove router env ref

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div>
         <Shell>
           <Switch>
